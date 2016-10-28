18de8f3345d4414ab16c08b162ddcc34958833f1
Matthias Scholz <devnull@localhost>
2016-10-28 11:22:11 +0200

    add license file

--- nlpca.m
+++ nlpca.m
@@ -42,7 +42,7 @@ function [pc, net, network] = nlpca(varargin)
 %  along with this program; if not, write to the Free Software
 %  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 %
-%  Copyright (C) 2006-2008 Matthias Scholz   
+%  Copyright (C) 2006-2016 Matthias Scholz   
 %  http://www.nlpca.org/matlab.html
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
